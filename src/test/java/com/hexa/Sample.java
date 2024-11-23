b06d5477ba43fbe2971713cd8c7b319d02170ec5
NitishGreens <nitishgreens@gmail.com>
2024-11-23 13:22:06 +0530

    My task done

---
+++ src/test/java/com/hexa/Sample.java
@@ -0,0 +1,17 @@
+package com.hexa;
+
+public class Sample {
+
+	public void test1() {
+		System.out.println("test1");
+	}
+
+	public void test2() {
+		System.out.println("test1");
+	}
+
+	public void test3() {
+		System.out.println("test1");
+	}
+
+}
